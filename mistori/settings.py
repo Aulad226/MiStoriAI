1e7d43759c81420a7feebb9834633c2c05705a02
Aulad226 <48339213+Aulad226@users.noreply.github.com>
2023-06-06 20:46:14 +0930

    Update settings.py

--- mistori/settings.py
+++ mistori/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n7gslr_^jz99cz)kmnxz&dnj3-he+1%bk^w5+9mcauptxjb5%t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
